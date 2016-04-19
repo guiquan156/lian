32d136675b6448ee641655ce349db81012bbffcf
lianguiquan <guiquan156@163.com>
2016-04-19 22:55:58 +0800

    git init

---
+++ app.js
@@ -0,0 +1,19 @@
+var http = require('http');
+var connect = require('connect');
+// var url = require('url');
+// var querystring = require('querystring');
+var app = connect();
+
+
+app.use('/', function(req, res){
+	var query = url.parse('http://www.baidu.com?a=hahaha&b=hehehe').query;
+	var parsedQuery = querystring.parse(query);
+	res.write('hello world');
+	res.end();
+});
+
+app.listen(3000);
+
+
+
+// http.listen(3000);
