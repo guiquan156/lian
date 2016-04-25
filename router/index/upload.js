7ea83c17195884359c693e0604e8df32942434ea
guiquan156 <guiquan156@163.com>
2016-04-25 10:45:48 +0800

    update

--- router/index/upload.js
+++ router/index/upload.js
@@ -25,21 +25,28 @@ module.exports = function(req, res){
 	form.maxFieldsSize = 10 * 1024 * 1024;
 
 
-	form.on('fileBegin', function(name, file){
-		for(var i=0; i<allowMIME.length; i++){
-			if(file.type === allowMIME[i]){
-				form.parse(req, function(error, fields, files){
-					fs.renameSync(files.upload.path, 'upload/test.html');
-					res.writeHead(200, {"content-type": "text/plain"});
-					res.write('upload success!!!');
-					res.end();
-				});
-			}else{
-				res.writeHead(200, {"content-type": "text/plain"});
-				res.write('failure!!!');
-				res.end();
-			}
-		}
+	// form.on('fileBegin', function(name, file){
+	// 	for(var i=0; i<allowMIME.length; i++){
+	// 		if(file.type === allowMIME[i]){
+	// 			form.parse(req, function(error, fields, files){
+	// 				fs.renameSync(files.upload.path, 'upload/test.html');
+	// 				res.writeHead(200, {"content-type": "text/plain"});
+	// 				res.write('upload success!!!');
+	// 				res.end();
+	// 			});
+	// 		}else{
+	// 			res.writeHead(200, {"content-type": "text/plain"});
+	// 			res.write('failure!!!');
+	// 			res.end();
+	// 		}
+	// 	}
+	// });
+
+	form.parse(req, function(error, fields, files){
+		fs.renameSync(files.upload.path, 'upload/test.html');
+		res.writeHead(200, {"content-type": "text/plain"});
+		res.write('upload success!!!');
+		res.end();
 	});
 
 
